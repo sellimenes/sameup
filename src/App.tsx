020247fd6833d4298c7dba57e7a372c6223b8ee9
Selim Enes <sellim81@gmail.com>
2024-02-09 21:22:41 +0300

    initial commit with shadcn

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from '@/components/ui/button'
+
+function App() {
+  return (
+    <>
+      <p>test</p>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
